e9f57adb310eb8e1bff50ea01a6d2f7849b8f386
Nic Ceynowa <nic.ceynowa@gmail.com>
2021-04-26 11:51:51 -0400

    first commmit

---
+++ README.md
@@ -0,0 +1,49 @@
+#aws_sts_mfa_gen
+Command line utility for generating and refreshing temporary IAM credentials for role assumption where MFA is required.
+See https://aws.amazon.com/premiumsupport/knowledge-center/authenticate-mfa-cli/ and https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-role.html for more details.
+## Installation
+- clone the repo
+- navigate to cloned directory, execute ```touch .env```
+- populate .env file with ```mfa_arn=$ARN_OF_YOUR_MFA_DEVICE```
+- ensure you are using python 3.8.2+ (use virtualenv, pyenv, etc.)
+- execute: ```pip install -r requirements```
+
+## Requirements
+A named profile under ~/.aws/credentials for your main account.  This profile must have permission to assume the role declared in the role profile.
+```
+[main]
+aws_access_key_id=REDACTED
+aws_secret_access_key=REDACTED
+region=us-east-1
+```
+
+A named profile that you will use in conjunction with the temporary credentials that you will generate.  
+You must supply the arn of the role that you want to assume.  Name this role relative to the account/role you will be working with. 
+
+```
+[role_profile]
+role_arn = REDACTED
+source_profile = temp_account_profile
+```
+
+##Usage
+Given the profiles configured above, execute:
+``` python cred_gen.py --token_code=$CODE_GENERATED_FROM_MFA_DEVICE --main_profile=main --temp_profile=temp_account_profile```
+
+
+This will generate a new profile within ~/.aws/credentials or update it in place, populated with:
+```
+[temp_account_profile]
+aws_access_key_id = REDACTED
+aws_secret_access_key = REDACTED
+aws_session_token = REDACTED
+```
+
+For clarity's sake, **this will update the profile entries if it already exists.**  make sure you are passing the command a profile you want to update.
+
+You can now use these temporary credentials to assume the role within the target account.  Ex:
+```aws s3 ls --profile=role_profile```
+
+
+
+
---
+++ cred_gen.py
@@ -0,0 +1,52 @@
+import boto3
+import argparse
+import subprocess
+import time
+from dotenv import dotenv_values
+
+#PROFILE TO MANIPULATE
+#[profile rearc-eng-mfa]
+#aws_access_key_id = return from sts call
+#aws_secret_access_key = return from sts call
+#aws_session_token = return from sts call
+
+class RearcSTSCredGen:
+    def __init__(self, token_code, main_profile, temp_cred_profile):
+        self.main_profile = main_profile
+        self.temp_cred_profile = temp_cred_profile
+        self.sts_client =  boto3.session.Session(profile_name=self.main_profile).client("sts")
+        self.config = dotenv_values(".env")
+        self.token_code = token_code
+
+    def session_token(self):
+        return self.sts_client.get_session_token(DurationSeconds=43200, SerialNumber=self.config["mfa_arn"],TokenCode=self.token_code)
+
+    def update_profile(self):
+        credentials = self.session_token()["Credentials"]
+        commands = [f"aws configure set aws_access_key_id {credentials['AccessKeyId']} --profile {self.temp_cred_profile};",
+            f"aws configure set aws_secret_access_key {credentials['SecretAccessKey']} --profile {self.temp_cred_profile};",
+            f"aws configure set aws_session_token {credentials['SessionToken']} --profile {self.temp_cred_profile}"]
+
+        for command in commands:
+            #need to sleep between to commands to make sure we include all entries under the same profile
+            #TODO
+            #would prefer to do this without subprocess, but the aws configure commmand updates profiles in place if they are there.
+            subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            time.sleep(1)
+
+def main():
+    parser = argparse.ArgumentParser(description='utility for updating temporary credentials to assume a role')
+    parser.add_argument('--token_code', dest="token_code", type=str,
+                        help='token code generated from your mfa device')
+    parser.add_argument('--main_profile', dest="main_profile", type=str,
+                        help="primary profile name to use.  should have credentials for your primary AWS account user")
+    parser.add_argument('--temp_profile', dest="temp_profile", type=str,
+                        help="temp profile name to use.  you will be able to use this profile to assume the role within the target account.")
+
+    args = parser.parse_args()
+
+    cred_gen = RearcSTSCredGen(token_code=args.token_code, main_profile=args.main_profile, temp_cred_profile=args.temp_profile)
+    cred_gen.update_profile()
+
+if __name__ == "__main__":
+    main()
